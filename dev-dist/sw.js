1c6572dece0a32457a9d54ecc3c728061b0425ce
tomate-adm <dumartinsmorais@gmail.com>
2025-12-31 01:30:34 -0300

    Update application configuration and assets

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.oist5jh6m0c"
+    "revision": "0.5j9f37044uk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
